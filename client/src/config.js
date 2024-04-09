2b379729e3bc93ad7ca0bcab2ce6f1ce1fd8078e
cansolele <androiddevelop@yandex.ru>
2024-04-09 15:35:37 +0300

    Docker

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 const config = Object.freeze({
-  apiURL: "http://192.168.1.1:5000", // Replace with your server address
+  apiURL: "http://server:5000", // Replace with your server address
 });
 
 export default config;
